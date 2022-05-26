033e33aeb69fbeafe8be910133229d64cc8d72d0
Sohan-Desai <105498548+Sohan-Desai@users.noreply.github.com>
2022-05-26 13:27:00 +0530

    Demonstrated Static datatypes and Constructor overloading.

--- My JAVA Project/src/Demo.java
+++ My JAVA Project/src/Demo.java
@@ -1,16 +1,19 @@
 
 public class Demo {
 	
+	static int i=0;
+	static int count=15;
+		
+	public static void CheckStatic() {
+		System.out.println(count +" + " +i +" = 15");	
+	}
+		
 	public static void main(String[] args) {
-		int i;
-		for (i=1; i>0; i++)
-		{
-			if(i%2!=0)
-				continue;
-			else if(i>20)
-				break;
-			System.out.println(i);
+		while (count>0) {
+			CheckStatic();
+			i++; count--;
 		}
+		
 	}
 
 }
