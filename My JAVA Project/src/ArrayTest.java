76238b89672661c7bd0d7e2681f060d5eb2fa3ba
Sohan-Desai <105498548+Sohan-Desai@users.noreply.github.com>
2022-05-28 02:09:14 +0530

    Dynamic array test with insertion, deletion, Expanding Array dynamically, binary search and shell sort.

---
+++ My JAVA Project/src/ArrayTest.java
@@ -0,0 +1,70 @@
+import java.util.Scanner;
+
+public class ArrayTest {
+	
+	int [] Arr;
+	int size, capacity;
+	
+	ArrayTest (int capacity){
+		this.capacity = capacity;
+		Arr = new int [capacity];
+		size = 0;
+	}
+	
+	void InsertElement() {
+		Scanner in = new Scanner(System.in);
+		int last, i;
+		System.out.println("How many Elements to enter?");
+		last = in.nextInt() +size;
+		for (i=size; i<last; i++) {
+			if (size==capacity)
+				ExpandArray();
+			Arr [i] = in.nextInt();
+			size++;
+		}
+	}
+	
+	void ExpandArray() {
+		int[] temp = new int[2*capacity];
+		for (int i=0; i<size; i++)
+			temp [i] = Arr [i];
+		Arr = temp;
+		capacity*=2;
+	}
+	
+	int DeleteElement() {
+		Scanner in = new Scanner (System.in);
+		System.out.println("Enter the index of element to be deleted: ");
+		int index = in.nextInt();
+		if (index>=size)
+			return -1;
+		int element = Arr[index];
+		while (index<size-1) {
+			Arr[index] = Arr[index+1];
+			index++;
+		}
+		Arr[index]=0;
+		size--;
+		return element;
+	}
+	
+	void Display() {
+		System.out.println("\nArray as per size,");
+		for (int i=0; i<size; i++)
+			System.out.print("\t" +Arr[i]);
+		System.out.println("\nArray as per capacity,");
+		for (int i=0; i<capacity; i++)
+			System.out.print("\t" +Arr[i]);
+		System.out.println();
+	}
+	
+	public static void main (String[] args) {
+		ArrayTest ob = new ArrayTest(4);
+		ob.InsertElement();
+		ob.Display();
+		System.out.println("\nDeleted Element = " +ob.DeleteElement());
+		ob.Display();
+		ob.InsertElement();
+		ob.Display();
+	}
+}
--- My JAVA Project/src/Demo.java
+++ My JAVA Project/src/Demo.java
@@ -1,19 +1,28 @@
-
+import java.util.Scanner;
 public class Demo {
-	
-	static int i=0;
-	static int count=15;
+	public static void main(String [] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number of elements to store :");
+		int limit = sc.nextInt();
+		int[] Array = new int [limit];
+		int gap,i,j,temp;
+		System.out.println("Enter the elements :");
+		for (i=0; i<limit; i++)
+			Array[i]=sc.nextInt();
 		
-	public static void CheckStatic() {
-		System.out.println(count +" + " +i +" = 15");	
-	}
-		
-	public static void main(String[] args) {
-		while (count>0) {
-			CheckStatic();
-			i++; count--;
+		for(gap=limit/2; gap>0; gap/=2) {
+			for(i=gap; i<limit; i++) {
+				for(j=i-gap; j>=0&&Array[j]>Array[j+gap]; j-=gap) {
+					temp = Array[j];
+					Array[j] = Array[j+gap];
+					Array[j+gap] = temp;
+				}
+			}
 		}
 		
+		System.out.println("Sorted array,");
+		for(j=0; j<limit; j++)
+			System.out.print("\t" +Array[j]);
+		System.out.println();
 	}
-
 }
